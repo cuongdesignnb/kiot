13e3d7eb7604f80121371937904a9d2c7ea93918
cuongdesign <dinhcuongdesign@gmail.com>
2026-03-26 16:09:18 +0700

    fix: POS crash, timezone GMT+7, quick-create product, fix Customers build error

--- resources/js/Pages/Customers/Index.vue
+++ resources/js/Pages/Customers/Index.vue
@@ -2390,15 +2390,6 @@ const submit = () => {
                                     </div>
                                 </div>
                             </div>
-                        </div>t
-                                    type="checkbox"
-                                    v-model="form.is_supplier"
-                                    class="sr-only peer"
-                                />
-                                <div
-                                    class="w-11 h-6 bg-gray-300 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"
-                                ></div>
-                            </label>
                         </div>
                     </form>
                 </div>
