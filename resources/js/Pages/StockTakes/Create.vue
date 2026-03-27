6ce1c29c88a707cf269d2c554339e8f4aedfec1d
cuongdesign <dinhcuongdesign@gmail.com>
2026-03-27 16:52:18 +0700

    fix: StockTakes/Create - add missing watch and axios imports causing runtime error

--- resources/js/Pages/StockTakes/Create.vue
+++ resources/js/Pages/StockTakes/Create.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { ref, computed } from 'vue';
+import { ref, computed, watch } from 'vue';
 import { Head, Link, router } from '@inertiajs/vue3';
+import axios from 'axios';
 
 const props = defineProps({
     products: Array,
